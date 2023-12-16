c2b44215056bc1b466e0a85c490aa77b0450350b
LeHa2505 <leha25052001@gmail.com>
2023-12-16 11:43:22 +0000

    update

--- app/controllers/controller.py
+++ app/controllers/controller.py
@@ -7,3 +7,9 @@ my_bp = Blueprint('my_blueprint')
 @my_bp.route('/my_bp')
 def my_bp_func(request):
 	return response.text('My First Blueprint')
+
+@my_bp.route('/ocr/<string:doc_type>/<string:doc_format>', methods={'GET'})
+def ocr_document(doc_type, doc_fomat) 
+
+	return jsonify(response_data)
+
