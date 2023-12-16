8705b1fbbbc6426aecb6128c33abe28d75ac35b7
LeHa2505 <leha25052001@gmail.com>
2023-12-16 14:18:39 +0000

    update 3

--- app/controllers/controller.py
+++ app/controllers/controller.py
@@ -2,10 +2,13 @@
 from sanic import response
 from sanic import Blueprint
 from sanic.request import Request
-import app.services.align_images
+from app.services.align_images import align_images
+from numpy import asarray
 import pytesseract
+import numpy as np
 import cv2
 import json
+import os
 
 
 def cleanup_text(text):
@@ -16,14 +19,14 @@ my_bp = Blueprint('my_blueprint')
 import requests
 from io import BytesIO
 from PIL import Image
+import urllib
 
 def download_image_from_url(url):
 	try:
-		response = requests.get(url)
-		response.raise_for_status()  # Raises an HTTPError if the HTTP request returned an unsuccessful status code
-
-		image = Image.open(BytesIO(response.content))
-		return image
+		url_response = urllib.request.urlopen(url)
+		img_array = np.array(bytearray(url_response.read()), dtype=np.uint8)
+		img = cv2.imdecode(img_array, -1)
+		return img
 	except Exception as e:
 		print(f"Error downloading image from URL: {e}")
 		return None
@@ -32,8 +35,8 @@ def load_template_and_config(doc_type):
 	try:
 		if doc_type == 1:
 			label = 'invoice_1'
-			template_folder = f'templates/invoices'
-			config_folder = f'classified/invoices'
+			template_folder = f'app/templates/invoices'
+			config_folder = f'app/classified/invoices'
 
 			# Load template image
 			template_image_path = os.path.join(template_folder, label + '.png')
@@ -75,72 +78,73 @@ async def ocr_document(request: Request):
 
 		if template is not None and config_data is not None:
 			image = download_image_from_url(image_url)
-			if image:
-				# Tiếp tục xử lý ảnh
-				# ...
-				print("[Info] aligning images...")
-				aligned = align_images.align_images(image, template)
+			print("img: ", image)
+			# if image:
+			# Tiếp tục xử lý ảnh
+			# ...
+			print("[Info] aligning images...")
+			aligned = align_images(image, template)
 
-				print("[Info] OCR'ing document...")
-				parsingResults = []
+			print("[Info] OCR'ing document...")
+			parsingResults = []
 
-				for loc in OCR_Locations:
-					bbox = loc["bbox"]
-					(x, y, w, h) = bbox
-					# roi = aligned[y:y+h, x:x+w]
-					roi = aligned[y: h, x: w]
+			for loc in OCR_Locations:
+				bbox = loc["bbox"]
+				(x, y, w, h) = bbox
+				# roi = aligned[y:y+h, x:x+w]
+				roi = aligned[y: h, x: w]
 
-					rgb = cv2.cvtColor(roi, cv2.COLOR_BGR2RGB)
-					text = pytesseract.image_to_string(rgb)
-					
-					for line in text.split("\n"):
-						if len(line) == 0:
-							continue
+				rgb = cv2.cvtColor(roi, cv2.COLOR_BGR2RGB)
+				text = pytesseract.image_to_string(rgb)
+				
+				for line in text.split("\n"):
+					if len(line) == 0:
+						continue
 
-						lower = line.lower()
-						count = sum([lower.count(x) for x in loc["filter_keywords"]])
+					lower = line.lower()
+					count = sum([lower.count(x) for x in loc["filter_keywords"]])
 
-						# if count == 0:
-						parsingResults.append((loc, line))
+					# if count == 0:
+					parsingResults.append((loc, line))
 
-				results = {}
+			results = {}
 
-				for (loc, line) in parsingResults:
-					print("loc:", loc)
-					r = results.get(loc['id'], None)
+			for (loc, line) in parsingResults:
+				print("loc:", loc)
+				r = results.get(loc['id'], None)
 
-					if r is None:
-						results[loc['id']] = (line, loc)
-					
-					else:
-						(existingText, loc) = r
-						text = "{}\n{}".format(existingText, line)
+				if r is None:
+					results[loc['id']] = (line, loc)
+				
+				else:
+					(existingText, loc) = r
+					text = "{}\n{}".format(existingText, line)
 
-						results[loc["id"]] = (text, loc)
+					results[loc["id"]] = (text, loc)
 
-				for (locID, result) in results.items():
-					(text, loc) = result
+			for (locID, result) in results.items():
+				(text, loc) = result
 
-					print(loc["id"])
-					print("=" * len(loc["id"]))
-					print("{}\n\n".format(text))
+				print(loc["id"])
+				print("=" * len(loc["id"]))
+				print("{}\n\n".format(text))
 
-					(x, y, w, h) = loc["bbox"]
-					clean = cleanup_text(text)
+				(x, y, w, h) = loc["bbox"]
+				clean = cleanup_text(text)
 
-					cv2.rectangle(aligned, (x, y), (x+w, y+h), (0, 255, 0), 2)
+				cv2.rectangle(aligned, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
-					for (i, line) in enumerate(clean.split("\n")):
-						startY = y + (i * 70) + 40
-						cv2.putText(aligned, line, (x, startY), cv2.FONT_HERSHEY_SIMPLEX, 1.8, (0, 0, 255), 5)
+				for (i, line) in enumerate(clean.split("\n")):
+					startY = y + (i * 70) + 40
+					cv2.putText(aligned, line, (x, startY), cv2.FONT_HERSHEY_SIMPLEX, 1.8, (0, 0, 255), 5)
 
-				cv2.imwrite("input.jpg", imutils.resize(image))
-				cv2.imwrite("Output.jpg", imutils.resize(aligned))
-				print("[Info] aligned image saved as new.jpg")
-				# cv2.imshow("Input", imutils.resize(image))
-				# cv2.imshow("Output", imutils.resize(aligned))
-				cv2.waitKey(0)
-				return response.text('Done')
+			cv2.imwrite("input.jpg", imutils.resize(image))
+			cv2.imwrite("Output.jpg", imutils.resize(aligned))
+			print("[Info] aligned image saved as new.jpg")
+			# cv2.imshow("Input", imutils.resize(image))
+			# cv2.imshow("Output", imutils.resize(aligned))
+			cv2.waitKey(0)
+			return response.text('Done')
 
 			# Perform OCR
 			# ...
